a4fbc41046dbb84d40189954dddb96b26ef21bbb
Iain Andrew <iainbruceandrew@gmail.com>
2014-07-04 22:18:28 +0100

    added arctext, started header bouncing animation

---
+++ app/scripts/main.js
@@ -0,0 +1,46 @@
+$(function() {
+
+    var $title = $('#header-title');
+
+    $title.arctext({
+        radius: -1
+    });
+
+    var titleArcUp = function() {
+
+        $title.arctext('set', {
+            radius      : 400, 
+            dir         : 1,
+            animation   : {
+                speed   : 300,
+                easing  : 'ease-in'
+            }
+        });
+        $title.animate({top:'40%'}, 300);
+
+    };
+
+    var titleArcDown = function() {
+
+        $title.arctext('set', {
+            radius      : -1, 
+            dir         : 1,
+            animation   : {
+                speed   : 600,
+                easing  : 'ease-out'
+            }
+        });
+        $title.animate({top:'50%'}, 600);
+
+    };
+
+    var titleArc = setInterval(function() {
+
+        titleArcUp();
+        setTimeout(function() {
+            titleArcDown();
+        }, 300);
+
+    }, 2000);
+
+});
