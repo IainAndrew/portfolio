327f63bfdc181f9ccc732cd0eecc5a1ec9d2bc7d
Iain Andrew <iainbruceandrew@gmail.com>
2014-07-07 21:40:41 +0100

    added zigzag font

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,52 +1,49 @@
-$(function() {
+var $title = $('#header-title'),
+    $shadow = $('.island-shadow');
 
-    var $title = $('#header-title'),
-        $shadow = $('.island-shadow');
-
-    $title.arctext({
-        radius: -1
-    });
-
-    var titleArcUp = function() {
-
-        $title.arctext('set', {
-            radius      : 400, 
-            dir         : 1,
-            animation   : {
-                speed   : 300,
-                easing  : 'ease-in'
-            }
-        });
-        $title.animate({top:'5%'}, 300);
-        $shadow.animate({width:'12rem'}, 300);
-
-    };
-
-    var titleArcDown = function() {
-
-        $title.arctext('set', {
-            radius      : -1, 
-            dir         : 1,
-            animation   : {
-                speed   : 600,
-                easing  : 'ease-out'
-            }
-        });
-        $title.animate({top:'20%'}, 600);
-        $shadow.animate({width:'15rem'}, 600);
-
-    };
-
-    var titleArc = setInterval(function() {
-
-        titleArcUp();
-        setTimeout(function() {
-            titleArcDown();
-        }, 300);
-
-    }, 2000);
-
+$title.arctext({
+    radius: -1
 });
+
+var titleArcUp = function() {
+
+    $title.arctext('set', {
+        radius      : 400, 
+        dir         : 1,
+        animation   : {
+            speed   : 300,
+            easing  : 'ease-in'
+        }
+    });
+    $title.animate({top:'5%'}, 300);
+    $shadow.animate({width:'12rem'}, 300);
+
+};
+
+var titleArcDown = function() {
+
+    $title.arctext('set', {
+        radius      : -1, 
+        dir         : 1,
+        animation   : {
+            speed   : 600,
+            easing  : 'ease-out'
+        }
+    });
+    $title.animate({top:'20%'}, 600);
+    $shadow.animate({width:'15rem'}, 600);
+
+};
+
+var titleArc = setInterval(function() {
+
+    titleArcUp();
+    setTimeout(function() {
+        titleArcDown();
+    }, 300);
+
+}, 2000);
+
 
 setTimeout(function() {
     
@@ -57,9 +54,30 @@ setTimeout(function() {
         $cloud3 = $('header .cloud-3');
 
     $trainBG.animate({opacity:1}, 1000);
-    $island.animate({top:'100%'}, 1000);
-    $cloud1.animate({left:'-100%'}, 2000);
-    $cloud2.animate({right:'-100%'}, 2000);
-    $cloud3.animate({left:'-100%'}, 2000);
+    $island.animate({top:'100%'}, 1000, function() {
+        $(this).hide();
+    });
+    $cloud1.animate({left:'-100%'}, 2000, function() {
+        $(this).hide();
+    });
+    $cloud2.animate({right:'-100%'}, 2000, function() {
+        $(this).hide();
+    });
+    $cloud3.animate({left:'-100%'}, 2000, function() {
+        $(this).hide();
+    });
 
-}, 6000);
+    $title.css({
+        'font-family': 'Helvetica',
+        color: '#00D7A6'
+    });
+
+    clearInterval(titleArc);
+
+}, 5500);
+
+setTimeout(function() {
+
+
+
+}, 7000);
