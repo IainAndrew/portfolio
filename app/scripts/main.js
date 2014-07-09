4f61ff4c8652c095adbc4fe1cef62b9ae891b4ab
Iain Andrew <iainbruceandrew@gmail.com>
2014-07-09 20:16:37 +0100

    added global setInterval function

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,122 +1,147 @@
-var $title = $('#header-title'),
-    $shadow = $('.island-shadow');
+var all = function() {
 
-$title.arctext({
-    radius: -1
-});
-
-var titleArcUp = function() {
-
-    $title.arctext('set', {
-        radius      : 400, 
-        dir         : 1,
-        animation   : {
-            speed   : 300,
-            easing  : 'ease-in'
-        }
-    });
-    $title.animate({marginTop:'-10%'}, 300);
-    $shadow.animate({width:'12rem'}, 300);
-
-};
-
-var titleArcDown = function() {
-
-    $title.arctext('set', {
-        radius      : -1, 
-        dir         : 1,
-        animation   : {
-            speed   : 600,
-            easing  : 'ease-out'
-        }
-    });
-    $title.animate({marginTop:0}, 600);
-    $shadow.animate({width:'15rem'}, 600);
-
-};
-
-var titleArc = setInterval(function() {
-
-    titleArcUp();
-    setTimeout(function() {
-        titleArcDown();
-    }, 300);
-
-}, 2000);
-
-
-setTimeout(function() {
-    
-    var $abstractBG = $('header .abstract-bg'),
+    var $title = $('#header-title'),
+        $shadow = $('.island-shadow'),
+        $abstractBG = $('header .abstract-bg'),
         $island = $('header .island-wrap'),
         $cloud1 = $('header .cloud-1'),
         $cloud2 = $('header .cloud-2'),
         $cloud3 = $('header .cloud-3');
 
-    $abstractBG.animate({opacity:1}, 1000);
-    $island.animate({top:'100%'}, 1000, function() {
-        $(this).hide();
-    });
-    $cloud1.animate({left:'-100%'}, 2000, function() {
-        $(this).hide();
-    });
-    $cloud2.animate({right:'-100%'}, 2000, function() {
-        $(this).hide();
-    });
-    $cloud3.animate({left:'-100%'}, 2000, function() {
-        $(this).hide();
+    $title.arctext({
+        radius: -1
     });
 
-    $title.addClass('header-abstract');
+    var titleArcUp = function() {
 
-    if (window.innerWidth <= 480) {
+        $title.arctext('set', {
+            radius      : 400, 
+            dir         : 1,
+            animation   : {
+                speed   : 300,
+                easing  : 'ease-in'
+            }
+        });
+        $title.animate({marginTop:'-10%'}, 300);
+        $shadow.animate({width:'12rem'}, 300);
 
-        $title.find('.char1').animate({top:'-15%', left:'8%'}, 1500);
-        $title.find('.char2').animate({top:'-15%', left:'18%'}, 1500);
-        $title.find('.char3').animate({top:'0', left:'15%'}, 1500);
-        $title.find('.char4').animate({top:'0', left:'20%'}, 1500);
-        $title.find('.char6').animate({top:'23%', left:'-10%'}, 1500);
-        $title.find('.char7').animate({top:'23%', left:'-11%'}, 1500);
-        $title.find('.char8').animate({top:'23%', left:'8%'}, 1500);
-        $title.find('.char9').animate({top:'50%', left:'-20%'}, 1500);
-        $title.find('.char10').animate({top:'50%', left:'-10%'}, 1500);
-        $title.find('.char11').animate({top:'50%', left:'-5%'}, 1500);
+    };
 
-    } else if (window.innerWidth > 480) {
+    var titleArcDown = function() {
 
-        $title.find('.char1').animate({top:'-15%', left:'0'}, 1500);
-        $title.find('.char2').animate({top:'-15%', left:'12%'}, 1500);
-        $title.find('.char3').animate({top:'0', left:'15%'}, 1500);
-        $title.find('.char4').animate({top:'0', left:'20%'}, 1500);
-        $title.find('.char6').animate({top:'23%', left:'-5%'}, 1500);
-        $title.find('.char7').animate({top:'23%', left:'-5%'}, 1500);
-        $title.find('.char8').animate({top:'23%', left:'14%'}, 1500);
-        $title.find('.char9').animate({top:'50%', left:'-15%'}, 1500);
-        $title.find('.char10').animate({top:'50%', left:'-5%'}, 1500);
-        $title.find('.char11').animate({top:'50%', left:'0%'}, 1500);
+        $title.arctext('set', {
+            radius      : -1, 
+            dir         : 1,
+            animation   : {
+                speed   : 600,
+                easing  : 'ease-out'
+            }
+        });
+        $title.animate({marginTop:0}, 600);
+        $shadow.animate({width:'15rem'}, 600);
 
-    }
+    };
 
-    $('.square').each(function(i) {
-        $(this).delay(2000).delay(i * 100).fadeIn();
-    });
+    var titleArc = setInterval(function() {
 
-    $('.zigzag').addClass('zigzag-appear');
+        titleArcUp();
+        setTimeout(function() {
+            titleArcDown();
+        }, 300);
 
-    //$('.char4').html('&#x25fc;');
+    }, 2000);
 
-    clearInterval(titleArc);
 
-}, 5500);
+    setTimeout(function() {
 
-setTimeout(function() {
+        $abstractBG.animate({opacity:1}, 1000);
 
-    $title.find('span').animate({top:0, left:0}, 1000);
-    //$title.delay(1000).animate({marginLeft:'-6rem'}, 500);
-    //$title.removeClass('header-abstract');
-    $title.css({
-        color:'white',
-        'font-family': 'helvetica'
-    });
+        $island.animate({top:'100%'}, 1000, function() {
+            $(this).hide();
+        });
+        $cloud1.animate({left:'-100%'}, 2000, function() {
+            $(this).hide();
+        });
+        $cloud2.animate({right:'-100%'}, 2000, function() {
+            $(this).hide();
+        });
+        $cloud3.animate({left:'-100%'}, 2000, function() {
+            $(this).hide();
+        });
 
-}, 12000);
+        $title.addClass('header-abstract');
+
+        if (window.innerWidth <= 480) {
+
+            $title.find('.char1').animate({top:'-15%', left:'8%'}, 1500);
+            $title.find('.char2').animate({top:'-15%', left:'18%'}, 1500);
+            $title.find('.char3').animate({top:'0', left:'15%'}, 1500);
+            $title.find('.char4').animate({top:'0', left:'20%'}, 1500);
+            $title.find('.char6').animate({top:'23%', left:'-10%'}, 1500);
+            $title.find('.char7').animate({top:'23%', left:'-11%'}, 1500);
+            $title.find('.char8').animate({top:'23%', left:'8%'}, 1500);
+            $title.find('.char9').animate({top:'50%', left:'-20%'}, 1500);
+            $title.find('.char10').animate({top:'50%', left:'-10%'}, 1500);
+            $title.find('.char11').animate({top:'50%', left:'-5%'}, 1500);
+
+        } else if (window.innerWidth > 480) {
+
+            $title.find('.char1').animate({top:'-15%', left:'0'}, 1500);
+            $title.find('.char2').animate({top:'-15%', left:'12%'}, 1500);
+            $title.find('.char3').animate({top:'0', left:'15%'}, 1500);
+            $title.find('.char4').animate({top:'0', left:'20%'}, 1500);
+            $title.find('.char6').animate({top:'23%', left:'-5%'}, 1500);
+            $title.find('.char7').animate({top:'23%', left:'-5%'}, 1500);
+            $title.find('.char8').animate({top:'23%', left:'14%'}, 1500);
+            $title.find('.char9').animate({top:'50%', left:'-15%'}, 1500);
+            $title.find('.char10').animate({top:'50%', left:'-5%'}, 1500);
+            $title.find('.char11').animate({top:'50%', left:'0%'}, 1500);
+
+        }
+
+        $('.square').each(function(i) {
+            $(this).delay(2000).delay(i * 100).fadeIn();
+        });
+
+        $('.zigzag').addClass('zigzag-appear');
+
+        //$('.char4').html('&#x25fc;');
+
+        clearInterval(titleArc);
+
+    }, 5500);
+
+    setTimeout(function() {
+
+        $title.find('span').animate({top:0, left:0}, {duration:1000, queue: false});
+        //$title.delay(1000).animate({marginLeft:'-6rem'}, 500);
+        $title.removeClass('header-abstract');
+
+        $('.square').each(function(i) {
+            $(this).delay(i * 100).fadeOut();
+        });
+
+        $abstractBG.animate({opacity:0}, 1000);
+
+        $('.zigzag').addClass('zigzag-disappear').removeClass('zigzag-appear');
+
+    }, 12000);
+
+    setTimeout(function() {
+
+        $island.show().animate({top:'37%'}, 1000);
+        $cloud1.show().animate({left:'2rem'}, 2000);
+        $cloud2.show().animate({right:'3rem'}, 2000);
+        $cloud3.show().animate({left:'1rem'}, 2000);
+
+        $('.zigzag').removeClass('zigzag-disappear');
+
+    }, 14000);
+};
+
+all();
+var allInterval = setInterval(function() {
+
+    all();
+
+}, 14000);
