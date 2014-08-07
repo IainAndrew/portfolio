85865e068acdfc1a40d2506aafa3edb9842daf7d
Iain Andrew <iainbruceandrew@gmail.com>
2014-08-07 14:11:05 +0100

    added iscroll - not yet working

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,9 +1,39 @@
 'use strict';
 
 
-$.stellar({
+(function(){
+  var ua = navigator.userAgent,
+    isMobileWebkit = /WebKit/.test(ua) && /Mobile/.test(ua);
+
+  if (isMobileWebkit) {
+    $('html').addClass('mobile');
+  }
+
+  $(function(){
+    var iScrollInstance;
+
+    if (isMobileWebkit) {
+      iScrollInstance = new iScroll('wrapper');
+
+      $('#scroller').stellar({
+        scrollProperty: 'transform',
+        positionProperty: 'transform',
+        horizontalScrolling: false,
+        verticalOffset: 150
+      });
+    } else {
+      $.stellar({
+        horizontalScrolling: false,
+        verticalOffset: 150
+      });
+    }
+  });
+
+})();
+
+/*$.stellar({
   horizontalScrolling: false,
-});
+});*/
 
 
 var all = function() {
